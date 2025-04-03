9f800c2eaceef0e91c5ada40a82c38abd93f782b
Justin8584 <zhou.jiad@northeastern.edu>
2025-04-03 16:48:12 -0700

    add bootsect and print

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Simple Makefile for our OS
 
-# Path to our cross-compiler
+# Path to our tools
 CC = x86_64-elf-gcc
 LD = x86_64-elf-ld
 ASM = nasm
@@ -9,12 +9,25 @@ QEMU = qemu-system-x86_64
 # First rule is the one executed when no parameters are fed to the Makefile
 all: run
 
+# Choose which boot sector to use
+# BOOTSECT = boot/boot_sect_memory.bin
+BOOTSECT = boot/boot_sect_main.bin
+
 # Rule to build the OS image
-os-image.bin: boot/boot_sect_hello.bin
+os-image.bin: $(BOOTSECT)
 	cat $^ > os-image.bin
 
-# Rule to build the boot sector
-boot/boot_sect_hello.bin: boot/boot_sect_hello.asm
+# # Rule to build the boot sectors
+# boot/boot_sect_memory.bin: boot/boot_sect_memory.asm
+# 	$(ASM) $< -f bin -o $@
+
+# boot/boot_sect_memory_org.bin: boot/boot_sect_memory_org.asm
+# 	$(ASM) $< -f bin -o $@
+
+# boot/boot_sect_stack.bin: boot/boot_sect_stack.asm
+# 	$(ASM) $< -f bin -o $@
+
+boot/boot_sect_main.bin: boot/boot_sect_main.asm boot/print.asm boot/print_hex.asm
 	$(ASM) $< -f bin -o $@
 
 # Rule to run our OS in QEMU
--- boot/boot_sect_memory.asm
+++ boot/boot_sect_memory.asm
@@ -1,23 +1,27 @@
-[org 0x7c00]
 mov ah, 0x0e
 
 ; attempt 1
-; Will fail again regardless of 'org' because we are still addressing the pointer
-; and not the data it points to
+; Fails because it tries to print the memory address (i.e. pointer)
+; not its actual contents
 mov al, "1"
 int 0x10
 mov al, the_secret
 int 0x10
 
 ; attempt 2
-; Having solved the memory offset problem with 'org', this is now the correct answer
+; It tries to print the memory address of 'the_secret' which is the correct approach.
+; However, BIOS places our bootsector binary at address 0x7c00
+; so we need to add that padding beforehand. We'll do that in attempt 3
 mov al, "2"
 int 0x10
 mov al, [the_secret]
 int 0x10
 
 ; attempt 3
-; As you expected, we are adding 0x7c00 twice, so this is not going to work
+; Add the BIOS starting offset 0x7c00 to the memory address of the X
+; and then dereference the contents of that pointer.
+; We need the help of a different register 'bx' because 'mov al, [ax]' is illegal.
+; A register can't be used as source and destination for the same command.
 mov al, "3"
 int 0x10
 mov bx, the_secret
@@ -26,9 +30,9 @@ mov al, [bx]
 int 0x10
 
 ; attempt 4
-; This still works because there are no memory references to pointers, so
-; the 'org' mode never applies. Directly addressing memory by counting bytes
-; is always going to work, but it's inconvenient
+; We try a shortcut since we know that the X is stored at byte 0x2d in our binary
+; That's smart but ineffective, we don't want to be recounting label offsets
+; every time we change the code
 mov al, "4"
 int 0x10
 mov al, [0x7c2d]
@@ -43,5 +47,5 @@ the_secret:
     db "X"
 
 ; zero padding and magic bios number
-times 510 - ($-$$) db 0
+times 510-($-$$) db 0
 dw 0xaa55
